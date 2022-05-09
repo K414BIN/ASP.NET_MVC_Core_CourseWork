b1328be68b55b7fb8dd95ff632368cece6b5c14e
k414bin <k414bin@yandex.ru>
2022-05-09 22:49:06 +0300

    Add controller

---
+++ CourseWork/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using CourseWork.Pages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace CourseWork
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(EmailModel model)
+        {
+            using (MailMessage message = new MailMessage(model.FromEmail,model.FromPassword))
+            {
+                message.Subject = model.Subject;
+                message.Body = model.Body;  
+                message.IsBodyHtml = false;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;  
+                    NetworkCredential cred = new NetworkCredential(model.FromEmail, model.FromPassword);
+                    smtp.UseDefaultCredentials = true;  
+                    smtp.Credentials = cred;    
+                    smtp.Send(message); 
+                    ViewBag.Message = "Email sent successfully.";
+                }
+            }
+            return View();
+        }
+
+        // GET: HomeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CourseWork/Pages/Email.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace CourseWork.Pages
+{
+    
+    public class EmailModel : PageModel
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; } 
+        public string FromEmail { get; set; }
+        public string FromPassword { get; set; }
+        public IFormFile Attachment { get; set; }       
+
+    }
+}
